ecd116345599312d033fe6d5d18935a4e52a17ee
Noh Ki <hae8573@koreatech.ac.kr>
2020-10-05 23:07:01 +0900

    version 1

---
+++ FactoryMethod-Asteroid-Lab-Student/src/AsteroidRectangleFactory.java
@@ -0,0 +1,15 @@
+/**
+ * @copyright 한국기술교육대학교 컴퓨터공학부 객체지향개발론및실습
+ * @version 2020년도 2학기 
+ * @author 2016136035 노기현
+ * 팩토리 메소드 패턴: Asteroid
+ * AsteroidDiamondFactory.java: 사각형 도형으로 소행성을 나타냄
+ */
+public class AsteroidRectangleFactory extends AsteroidFactory {
+
+	@Override
+	protected Double[] createPoints(Location centerLoc, int radius) {
+		return null;
+	}
+
+}
